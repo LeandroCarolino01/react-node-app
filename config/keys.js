148c7bed2c3ca9d96d426f4307ad9b8ae65f494a
Leandro.Carolino <carolino.leandro@gmail.com>
2018-11-03 16:00:40 +0100

    config file added and user created

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://leandro:leandro01@ds151453.mlab.com:51453/devconnector'
+}
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// db config
+const db = require('./config/keys').mongoURI;
 
-const port = process.env.port || 5000;
+//connect to mongodb
+mongoose
+    .connect(db)
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello world!!'));
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
